53b5b3993ed646497143681573ea79d90fcc7b7e
Christina Lebron <cmlebron14@gmail.com>
2021-07-08 16:31:40 -0400

    javascript tools

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,19 +1,31 @@
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
-      React.createElement("h1", {id: "logo"}, "Adopt Me!"),
-      React.createElement(Pet, {name: "Max", animal: "Cat", breed: "Arabian Mau"}),
-      React.createElement(Pet, {name: "Leo", animal: "Cat", breed: "Arabian Mau"}),
-      React.createElement(Pet, {name: "Sherman", animal: "Dog", breed: "Pug"}),
-      React.createElement(Pet, {name: "Penny", animal: "Dog", breed: "Pug"}),
-    ]);
+    React.createElement("h1", { id: "logo" }, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Max",
+      animal: "Cat",
+      breed: "Arabian Mau",
+    }),
+    React.createElement(Pet, {
+      name: "Leo",
+      animal: "Cat",
+      breed: "Arabian Mau",
+    }),
+    React.createElement(Pet, {
+      name: "Sherman",
+      animal: "Dog",
+      breed: "Pug",
+    }),
+    React.createElement(Pet, {
+      name: "Penny",
+      animal: "Dog",
+      breed: "Pug",
+    }),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
